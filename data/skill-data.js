60d3599f8d5dfced96c3fc53145ebfcc32095203
nathanth <nathan.hardenburg@icloud.com>
2022-06-16 09:13:19 -0500

    connects app to MongoDB

--- data/skill-data.js
+++ data/skill-data.js
@@ -2,6 +2,11 @@ const skills = [
   {type: 'HTML', proficiency: 'proficient', _id: 125223},
   {type: 'CSS', proficiency: 'proficient', _id: 127904},
   {type: 'JS', proficiency: 'proficient', _id: 139608},
+  {type: 'Mongoose', proficiency: 'proficient', _id: 139608},
+  {type: 'Express', proficiency: 'proficient', _id: 139608},
+  {type: 'MongoDB', proficiency: 'proficient', _id: 139608},
+  {type: 'Ruby', proficiency: 'not proficient', _id: 139608},
+  {type: 'Python', proficiency: 'not proficient', _id: 139608},
 ]
 
 export {
